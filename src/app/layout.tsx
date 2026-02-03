bfe7df31e4b275206df86405dd19366de473acc0
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2026-02-03 22:16:30 +0100

    import global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Footer, SideBar } from '@/components';
 import { SidebarProvider } from '@/components/ui/sidebar';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
